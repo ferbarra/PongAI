1f5caee1cc89e1b4d1d51b25ecfc98e5aef69462
ferbarra2207 <fernandobarra2207@gmail.com>
2016-10-11 12:11:38 -0600

    Adds draw function to Ball class

--- pong.py
+++ pong.py
@@ -2,7 +2,8 @@ import pygame
 
 def main():
     surface, size = create_window()
-    
+    pong_ball = Ball([200, 200], 20, (255,255,255), surface)
+    pong_ball.draw()
     
 def create_window():
     title = 'Ping Pong'
@@ -11,8 +12,21 @@ def create_window():
     pygame.display.set_caption('Ping Pong')
     return surface, size
 
-#def start_match():
+# User-definded classes:
+
+class Ball:
+    def __init__(self, center, radius, color, surface):
+        self.center = center
+        self.radius = radius
+        self.color = color
+        self.surface = surface
     
+    def draw(self):
+        # Draw circle
+        pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+    
+
+
 
 class Player:
     def __init__(self, x_coor, y_coor):
@@ -21,7 +35,6 @@ class Player:
         self.width = 15
         self.height = 100
         self.color = 0,0,0
-    
-        
+        self.surface = surface
 
 main()
