4c949961573df3d6ee325d9fa903f606261ec863
ferbarra2207 <fernandobarra2207@gmail.com>
2016-10-14 11:18:47 -0600

    Adds Player class

--- pong.py
+++ pong.py
@@ -32,13 +32,25 @@ class Ball:
         for index in range(0, 2):
             self.center[index] = (self.center[index] + 1) % surface_size[index]  
             
-          
+class Player:
+    def __init__(self, coordinates, width, height, color, surface):
+        self.coordinates = coordinates
+        self.width = width
+        self.height = height
+        self.color = color
+        self.surface = surface
+        
+    def draw(self):
+        # Draw rectangle
+        pygame.draw.rect(self.surface, self.color, (self.coordinates[0],self.coordinates[1], self.width, self.height))
         
 class Game:
     def __init__(self, surface):
         self.surface = surface
         self.bg_color = pygame.Color('black')
-        self.ball = Ball([200, 200], 20, pygame.Color('white'), surface)
+        self.fg_color = pygame.Color('white')
+        self.ball = Ball([200, 200], 20, self.fg_color, self.surface)
+        self.player_1 = Player([10,200], 20, 100, self.fg_color, self.surface)
         self.close_clicked = False      
         
     def play(self):
@@ -61,7 +73,9 @@ class Game:
     def draw(self):
         self.surface.fill(self.bg_color)
         self.ball.draw()
+        self.player_1.draw()
         # Get screen size
+        """
         surface_size = self.surface.get_size()
         surface_width = surface_size[0]
         surface_height = surface_size[1]
@@ -69,6 +83,8 @@ class Game:
         player_height = 100
         pygame.draw.rect(self.surface, pygame.Color('white'), (10,200,player_width, player_height))
         pygame.draw.rect(self.surface, pygame.Color('white'), (surface_width - (player_width + 10),200,player_width, player_height))
+        """        
         pygame.display.update()    
+        
 
 main()
